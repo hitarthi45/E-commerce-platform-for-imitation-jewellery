cb488863d454179cd515ef780b88905a424b877f
Hitarthi_45 <149648663+hitarthi45@users.noreply.github.com>
2026-03-20 08:55:20 +0530

    Add files via upload

---
+++ Backend/routes/rts_auth.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/model_user");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    // check existing user
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    res.json({ message: "User registered successfully", user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
+
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    // token
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      "secretkey",
+      { expiresIn: "1d" }
+    );
+
+    res.json({ message: "Login successful", token, user });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
